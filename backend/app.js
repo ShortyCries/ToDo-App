95f8ba8a14e286ed778aeb6a355ef101922adfd5
jamilshehab <jamilshehab>
2025-04-28 16:22:53 +0300

    testing node js on the server

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ const PORT = 3000;
 //connect to mongodb
 connectToMongoDb();
 //
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 //listen to port current port
 app.listen(PORT, () => {
   console.log(`connected successfully listen to port at ${PORT}`);
