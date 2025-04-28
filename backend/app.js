b3caab2cdde888a410daff0a478db1b88beab384
jamilshehab <jamilshehab>
2025-04-28 14:27:13 +0300

    fixing errors setting up express and node

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//connect to mongodb
+
+//
+
+//
+app.listen(PORT, () => {
+  console.log("connected successfully");
+});
