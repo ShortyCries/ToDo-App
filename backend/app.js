6d3f65db133fa94ae7c1ad3a462331e81de62d00
jamilshehab <jamilshehab>
2025-04-28 16:17:09 +0300

    connected to the database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { connectToMongoDb } = require("./db/db");
 const app = express();
 const PORT = 3000;
 
 //connect to mongodb
-
+connectToMongoDb();
 //
 
 //listen to port current port
